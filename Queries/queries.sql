0cb72edae6dec955836266ef244bb4b937829c62
pharmeac <emily@Emilys-MacBook-Pro.local>
2022-10-08 12:32:41 -0400

    sales dev emp info csvs added, updated queries

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -93,3 +93,61 @@ GROUP BY de.dept_no
 ORDER BY de.dept_no;
 
 Select * from countbydept_no
+
+Select * from salaries
+ORDER BY to_date DeSC;
+
+SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary, de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_employee as de
+ON (e.emp_no = de.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+AND (de.to_date = '9999-01-01');
+
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
+		
+DROP TABLE manager_info;
+
+SELECT ce.emp_no,
+	ce.first_name,
+	ce.last_name,
+	d.dept_name
+-- INTO dept_info
+FROM current_emp as ce
+INNER JOIN dept_employee AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN departments AS d
+ON (de.dept_no = d.dept_no);
+
+Select * from retirement_info
+
+Select e.emp_no as "Employee numbers", 
+e.first_name as "First Name", 
+e.last_name as "Last Name", 
+d.dept_name as "Department Name" 
+INTO salesanddevelopment_retirement_info
+FROM emp_info as e
+LEFT JOIN dept_employee as de
+ON (e.emp_no = de.emp_no)
+LEFT JOIN departments as d
+ON (de.dept_no = d.dept_no)
+WHERE de.to_date = '9999-01-01'
+AND d.dept_name in ('Sales', 'Development')
+ORDER BY e.last_name, e.first_name;
