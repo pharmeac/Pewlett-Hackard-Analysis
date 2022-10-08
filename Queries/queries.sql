57049e7f2691fb3b67675bd3b944495938649d00
pharmeac <emily@Emilys-MacBook-Pro.local>
2022-10-08 11:28:54 -0400

    queries updated, countbydept csv added

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -40,3 +40,56 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 Select * from retirement_info
 
 DROP TABLE retirement_info
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no;
+
+SELECT ri.emp_no,
+    ri.first_name,
+	ri.last_name,
+    de.to_date
+FROM retirement_info AS ri
+LEFT JOIN dept_employee AS de
+ON ri.emp_no = de.emp_no;
+
+--join ri and de tables to get emp no, last name, first name, to date
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_employee as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number
+DROP TABLE retirecountbydept_no
+
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO countbydept_no
+FROM current_emp as ce
+LEFT JOIN dept_employee as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
+
+Select * from countbydept_no
